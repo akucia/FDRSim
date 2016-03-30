d676f3ab72720ef90eb6060135c61c4cebf597d2
Kuciu <a.kucia@onet.eu>
2016-03-30 13:32:53 +0200

    simple test gui

--- FDR_GUI.py
+++ FDR_GUI.py
@@ -1,32 +1,33 @@
-
-import matplotlib
-matplotlib.use("TkAgg")
-from matplotlib.backends.backend_tkagg import Figure
-
 import Tkinter as tk
-import ttk
+
 LARGE_FONT=("Verdana",12)
+
+def NI():
+    print("Not implemented yet!")
+
+def qf(param):
+    print(param)
+
 
 class FDR_GUI(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
-
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
-        container.grid_rowconfigure(0, weight=1)
+        container.grid_rowconfigure(1, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
 
-        for F in (StartPage, PageOne, PageTwo):
+        for F in (StartPage, CollectorPage, AnalyzerPage):
 
             frame = F(container, self)
 
             self.frames[F] = frame
 
-            frame.grid(row=1,column=0, sticky="nsew")
+            frame.grid(row=1, column=0, sticky="nsew")
 
         self.show_frame(StartPage)
 
@@ -36,60 +37,59 @@ class FDR_GUI(tk.Tk):
         frame.tkraise()
 
 
-def qf(param):
-    print(param)
-
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
-        label = ttk.Label(self, text="StartPage", font=LARGE_FONT)
+        label = tk.Label(self, text="Start", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button1 = ttk.Button(self, text="Visit Page 1",
-                            command=lambda: controller.show_frame(PageOne))
+        button1 = tk.Button(self, text="Collect Data",
+                            command=lambda: controller.show_frame(CollectorPage))
         button1.pack()
 
-        button2 = ttk.Button(self, text="Visit Page 2",
-                            command=lambda: controller.show_frame(PageTwo))
+        button2 = tk.Button(self, text="Analyze Data",
+                            command=lambda: controller.show_frame(AnalyzerPage))
         button2.pack()
 
+        button3 = tk.Button(self, text="Quit",
+                            command=lambda: self.quit() )
+        button3.pack()
 
-class PageOne(tk.Frame):
+
+class CollectorPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
-        label = ttk.Label(self, text="Page 1", font=LARGE_FONT)
+        label = tk.Label(self, text="Data Collector", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button1 = ttk.Button(self, text="Back to home",
+        button2 = tk.Button(self, text="STH", command= lambda : NI())
+
+        button2.pack()
+
+        button1 = tk.Button(self, text="Back to home",
                             command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
-        button2 = ttk.Button(self, text="Page 2",
-                            command=lambda: controller.show_frame(PageTwo))
-        button2.pack()
 
-class PageTwo(tk.Frame):
+class AnalyzerPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
-        label = ttk.Label(self, text="Page 2", font=LARGE_FONT)
+        label = tk.Label(self, text="Data Analyzer", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button1 = ttk.Button(self, text="Back to home",
+        button2 = tk.Button(self, text="STH",
+                            command=lambda: NI())
+        button2.pack()
+
+        button1 = tk.Button(self, text="Back to home",
                             command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
-        button2 = ttk.Button(self, text="Page One",
-                            command=lambda: controller.show_frame(PageOne))
-        button2.pack()
-
-class PageThree(tk.Frame):
-    pass
 
 app = FDR_GUI()
 app.mainloop()
-
